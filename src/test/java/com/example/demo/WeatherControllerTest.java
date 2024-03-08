391ce7755cb7c831877d381bf4277b2502f9e38f
Julien Dubois <julien.dubois@gmail.com>
2024-03-08 12:22:33 +0100

    Update tests

--- src/test/java/com/example/demo/WeatherControllerTest.java
+++ src/test/java/com/example/demo/WeatherControllerTest.java
@@ -1,7 +1,6 @@
 package com.example.demo;
 
 import static org.hamcrest.Matchers.containsString;
-import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -11,7 +10,6 @@ import org.junit.jupiter.api.Test;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 @WebMvcTest(WeatherController.class)
