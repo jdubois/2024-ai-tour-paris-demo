41a568b9d32e5d999672056afb0866bc58413ade
Julien Dubois <julien.dubois@gmail.com>
2024-03-12 11:09:57 +0100

    Remove unused import

--- src/test/java/com/example/demo/WeatherControllerTest.java
+++ src/test/java/com/example/demo/WeatherControllerTest.java
@@ -13,8 +13,6 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 
-import com.example.demo.model.*;
-
 @SpringBootTest
 @AutoConfigureMockMvc
 class WebMockTest {
